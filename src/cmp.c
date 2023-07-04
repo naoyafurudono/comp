62250b7737f7ada41f430c478ea1d0775dd3773a
Naoya Furudono <naoyafurudono@gmail.com>
2023-07-05 01:20:13 +0900

    step2

--- src/cmp.c
+++ src/cmp.c
@@ -1,5 +1,95 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+  TK_RESERVED,
+  TK_NUM,
+  TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+  TokenKind kind;
+  Token *next;
+  int val;
+  char *str;
+};
+
+Token *token;
+
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+bool eat(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op) {
+    return false;
+  }
+  token = token->next;
+  return true;
+}
+
+void must_eat(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op) {
+    error("'%c'ではありません", op);
+  }
+  token = token->next;
+}
+
+int must_number() {
+  if (token->kind != TK_NUM) {
+    error("数ではありません");
+  }
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+bool at_eof() {
+  return token->kind == TK_EOF;
+}
+
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  cur->next = tok;
+  return tok;
+}
+
+Token *tokenize(char *p) {
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+      if (isspace(*p)) {
+        p++;
+        continue;
+      }
+      if (*p == '+' || *p == '-') {
+        cur = new_token(TK_RESERVED, cur, p++);
+        continue;
+      }
+      if (isdigit(*p)) {
+        cur = new_token(TK_NUM, cur, p);
+        cur->val = strtol(p, &p, 10);
+        continue;
+      }
+      error("トークナイズできません");
+  }
+  new_token(TK_EOF, cur, p);
+  return head.next;
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -7,27 +97,26 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  char *p = argv[1];
+  token = tokenize(argv[1]);
 
   printf(".globl _main\n");
   printf(".text\n");
   printf(".balign 4\n");
   printf("_main:\n");
-  printf("    mov w0, #%ld\n", strtol(p, &p, 10));
-  while(*p){
-    if (*p == '+') {
-      p++;
-      printf("    add w0, w0, #%ld\n", strtol(p, &p, 10));
+  printf("    mov w0, #%d\n", must_number());
+  while(!at_eof()){
+
+    if (eat('+')) {
+      printf("    add w0, w0, #%d\n", must_number());
       continue;
     }
 
-    if (*p == '-') {
-      p++;
-      printf("    sub w0, w0, #%ld\n", strtol(p, &p, 10));
+    if (eat('-')) {
+      printf("    sub w0, w0, #%d\n", must_number());
       continue;
     }
 
-    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+    fprintf(stderr, "予期しない文字です: '%c'\n", token->str[0]);
     return 1;
   }
   printf("    ret\n");
