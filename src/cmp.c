b37694f13c9cec4f66f2cd3c68748a4d68df8d00
Naoya Furudono <naoyafurudono@gmail.com>
2023-07-05 22:14:48 +0900

    improve erorr message

--- src/cmp.c
+++ src/cmp.c
@@ -5,7 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef enum {
+typedef enum
+{
   TK_RESERVED,
   TK_NUM,
   TK_EOF,
@@ -13,52 +14,17 @@ typedef enum {
 
 typedef struct Token Token;
 
-struct Token {
-  TokenKind kind;
-  Token *next;
-  int val;
-  char *str;
+struct Token
+{
+  TokenKind kind; // must
+  Token *next;    // must
+  int val;        // optional
+  char *str;      // must
 };
 
 Token *token;
-
-void error(char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  vfprintf(stderr, fmt, ap);
-  fprintf(stderr, "\n");
-  exit(1);
-}
-
-bool eat(char op) {
-  if (token->kind != TK_RESERVED || token->str[0] != op) {
-    return false;
-  }
-  token = token->next;
-  return true;
-}
-
-void must_eat(char op) {
-  if (token->kind != TK_RESERVED || token->str[0] != op) {
-    error("'%c'ではありません", op);
-  }
-  token = token->next;
-}
-
-int must_number() {
-  if (token->kind != TK_NUM) {
-    error("数ではありません");
-  }
-  int val = token->val;
-  token = token->next;
-  return val;
-}
-
-bool at_eof() {
-  return token->kind == TK_EOF;
-}
-
-Token *new_token(TokenKind kind, Token *cur, char *str) {
+Token *new_token(TokenKind kind, Token *cur, char *str)
+{
   Token *tok = calloc(1, sizeof(Token));
   tok->kind = kind;
   tok->str = str;
@@ -66,37 +32,104 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
   return tok;
 }
 
-Token *tokenize(char *p) {
+void error(char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+char *user_input;
+void error_at(char *loc, char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " ");
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+bool eat(char op)
+{
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+  {
+    return false;
+  }
+  token = token->next;
+  return true;
+}
+
+void must_eat(char op)
+{
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+  {
+    error_at(token->str, "'%c'ではありません", op);
+  }
+  token = token->next;
+}
+
+int must_number()
+{
+  if (token->kind != TK_NUM)
+  {
+    error_at(token->str, "数ではありません");
+  }
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+bool at_eof()
+{
+  return token->kind == TK_EOF;
+}
+
+Token *tokenize(char *p)
+{
   Token head;
   head.next = NULL;
   Token *cur = &head;
 
-  while (*p) {
-      if (isspace(*p)) {
-        p++;
-        continue;
-      }
-      if (*p == '+' || *p == '-') {
-        cur = new_token(TK_RESERVED, cur, p++);
-        continue;
-      }
-      if (isdigit(*p)) {
-        cur = new_token(TK_NUM, cur, p);
-        cur->val = strtol(p, &p, 10);
-        continue;
-      }
-      error("トークナイズできません");
+  while (*p)
+  {
+    if (isspace(*p))
+    {
+      p++;
+      continue;
+    }
+    if (*p == '+' || *p == '-')
+    {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+    if (isdigit(*p))
+    {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+    error_at(p, "トークナイズできません");
   }
   new_token(TK_EOF, cur, p);
   return head.next;
 }
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
+int main(int argc, char **argv)
+{
+  if (argc != 2)
+  {
     fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
   }
 
+  user_input = argv[1];
   token = tokenize(argv[1]);
 
   printf(".globl _main\n");
@@ -104,20 +137,22 @@ int main(int argc, char **argv) {
   printf(".balign 4\n");
   printf("_main:\n");
   printf("    mov w0, #%d\n", must_number());
-  while(!at_eof()){
+  while (!at_eof())
+  {
 
-    if (eat('+')) {
+    if (eat('+'))
+    {
       printf("    add w0, w0, #%d\n", must_number());
       continue;
     }
 
-    if (eat('-')) {
+    if (eat('-'))
+    {
       printf("    sub w0, w0, #%d\n", must_number());
       continue;
     }
 
-    fprintf(stderr, "予期しない文字です: '%c'\n", token->str[0]);
-    return 1;
+    error_at(token->str, "予期しない文字です: %c %n", token->str);
   }
   printf("    ret\n");
   return 0;
